1deff4e2cfe87a8befba09c60bed9f0c47f3235f
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-06-13 00:31:01 +0200

    Quelques vagues idées

--- inprogress.py
+++ inprogress.py
@@ -1,4 +1,50 @@
+#Il faut récupérer la page demandée
+recuppage
+wget $cible ou, mieux, cf la vidéo proposée par MBA
+#Il faut en extraire les liens, attention aux chemins relatifs
+recupliens
 from bs4 import BeautifulSoup
 import csv
 
-soup = BeautifulSoup(open("")) //ici il faut passer la page à traiter via une variable.
+#en extraire les mots avec BeautifulSoup
+recupmots
+#sauvegarder cela dans un XML
+#hypothèse
+<page>
+  <mots>
+    <entree>
+        <mot>
+        </mot>
+        <pourcentage>
+        </pourcentage>
+    <entree>
+  </mots>
+  <liens>
+    <adresse>
+    </adresse>
+  </liens>
+<page>
+
+#on stocke dans la BDD
+
+
+#ensuite on boucle. D'abord en horizontal, donc niveau par niveau, plutôt que de descendre en profondeur dans l'arborescence des liens. Non, peut-être mieux en profondeur d'abord. Tout ce qui suit est à revoir
+
+pour adresse in liens[]
+    recuppage,
+    recupmots,
+    BDD
+    IDpagestartniveau1
+    IDpageendniveau1
+    pour IDpagestartniveau1 <= X <= IDpageendniveau1
+        pour adresse in liens[]
+            recuppage,
+            recupmots,
+            BDD
+            IDpagestartniveau2
+            IDpageendniveau2
+        finpour
+    finpour
+finpour
+
+Comment boucler les niveaux successifs? Par le biais de l'ID page: quand on crée
