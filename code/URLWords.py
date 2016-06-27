d60ba3c6cb3db5c8a57eab5fe5141292b90dcd04
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-06-27 12:49:23 +0200

    Idée de la classe pour stocker les résultats des analyses

---
+++ code/URLWords.py
@@ -0,0 +1,12 @@
+class URLWords(object):
+    '''
+    Objet pour traiter les résultats des analyses des pages.
+
+    Attributs:
+       wordcount: un entier pour le total des mots.
+       wordsitems: une liste avec mots, leur occurrence, leur pourcentage (["salut", 3, 42.86], ["bonjour",  2, 28.57], ["adieu", 2, 28.57])
+    '''
+
+    def __init__(self, ):
+        '''
+        '''
