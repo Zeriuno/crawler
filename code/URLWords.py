ad3c273e55438ea4e29abf241c3d8dd44d98941a
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-06-30 00:00:41 +0200

    Objet pour les résutats

--- code/URLWords.py
+++ code/URLWords.py
@@ -2,11 +2,19 @@ class URLWords(object):
     '''
     Objet pour traiter les résultats des analyses des pages.
 
+
     Attributs:
        wordcount: un entier pour le total des mots.
        wordsitems: une liste avec mots, leur occurrence, leur pourcentage (["salut", 3, 42.86], ["bonjour",  2, 28.57], ["adieu", 2, 28.57])
+       URL : 
     '''
 
-    def __init__(self, ):
+    def __init__(self):
+        '''
+        '''
+
+class WordsItems(object):
+
+    def __init__(self):
         '''
         '''
