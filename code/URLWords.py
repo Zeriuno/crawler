4134f5f51490b3a3c49ad7e5b7504b4f88fb5846
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-06-30 17:18:31 +0200

    fonction d'initialisation de l'objet URLWords

--- code/URLWords.py
+++ code/URLWords.py
@@ -9,10 +9,13 @@ class URLWords(object):
         wordsitems: une liste avec mots, leur occurrence, leur pourcentage (["salut", 3, 42.86], ["bonjour",  2, 28.57], ["adieu", 2, 28.57])
     '''
 
-    def __init__(self):
+    def __init__(self, url):
         '''
+        Pour créer un objet URLWords en passant l'URL en paramètre (la première valeur qu'on a, on la stocke donc directement).
+        Exemple d'utilisation:
+        analysePage = URLWords(url)
         '''
-
+        self.address = url
 class WordsItems(object):
 
     def __init__(self):
