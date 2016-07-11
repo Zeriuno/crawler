4bc1cb13f1641668b7959128677adb0ca00a17ef
MIMOC2016 <meriyamabane@gmail.Com>
2016-07-11 15:37:41 +0200

    récupérer le lien ok

--- site crawler/web/cherrypy/crawler.py
+++ site crawler/web/cherrypy/crawler.py
@@ -4,11 +4,19 @@ from jinja2 import Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader('templates'))
 
+#affichage de la page index.html
+
 class Crawler(object):
+    @cherrypy.expose
     def index(self):
         tmpl = env.get_template('index.html')
         return tmpl.render()
 
     index.exposed = True
 
+    def prendreURL (self, lien=None):
+        return lien
+    prendreURL.exposed = True
+
+#demarrage du serveur cherrypy
 cherrypy.quickstart(Crawler(), config='server.conf')
