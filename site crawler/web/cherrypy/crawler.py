fb6db21ab64de0be793bffb9218fa26f913e2e55
MIMOC2016 <meriyamabane@gmail.Com>
2016-07-11 16:20:33 +0200

    parser la page?
    Help

--- site crawler/web/cherrypy/crawler.py
+++ site crawler/web/cherrypy/crawler.py
@@ -2,6 +2,9 @@ import cherrypy
 import pymysql
 from jinja2 import Environment, FileSystemLoader
 
+import requests
+from bs4 import BeautifulSoup
+
 env = Environment(loader=FileSystemLoader('templates'))
 
 #affichage de la page index.html
@@ -14,9 +17,22 @@ class Crawler(object):
 
     index.exposed = True
 
+#récupérer le lien soumis
     def prendreURL (self, lien=None):
         return lien
     prendreURL.exposed = True
+
+#recupérer le texte de la page
+
+    def grabpage(lien):
+        '''
+        Prend l'URL passsée en paramètre et la récupère avec requests.
+        On peut l'utiliser de cette manière: soup = grabpage(url)
+        '''
+        r = requests.get(lien)
+        soup = BeautifulSoup(r.content, "html.parser")
+        return soup
+    grabpage.exposed = True
 
 #demarrage du serveur cherrypy
 cherrypy.quickstart(Crawler(), config='server.conf')
