a0dc71a01becceba25c7b1ca91055403bb3fa6da
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-07-12 18:29:08 +0200

    Fonction pour compter les mots dans la classe Page

--- site crawler/code/Page.py
+++ site crawler/code/Page.py
@@ -20,3 +20,11 @@ class Page:
             url = link.get('href')
             if(url not in self.links):
                 self.links.append(url)
+
+    def wordcount():
+        '''
+        Fonction qui construit le compte des mots de la page. Elle est appellée avec `NomObjetPage.wordcount()`
+        '''
+        words = self.soup.get_text #On récupère le texte
+        items = [word.replace('"', '').lower() for word in words.split()] #découpage en mots, imparfait: les mots avec apostrophe restent unis.
+        itemsfreqs = sorted([(items.count(word), word) for word in set(items)], reverse=True) #dans itemsfreqs on a ainsi une liste d'éléments constitués de nombre d'occurrences et mot, la liste est ordonnée par nombre décroissant d'occurrences.
