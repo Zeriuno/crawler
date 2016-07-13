6fe2aed292dbc47df862f6a2b5355f2cdfa7d32e
MIMOC2016 <meriyamabane@gmail.Com>
2016-07-13 18:37:10 +0200

    enregistrer les 3 1er mots dans le fichier

--- site crawler/web/cherrypy/crawler.py
+++ site crawler/web/cherrypy/crawler.py
@@ -21,14 +21,13 @@ class Crawler(object):
     def prendreURL (self, lien=None):
         if lien:
             #ici nous allons dérouler tout notre programme. Ce sera notre "main"
-            print("Crawling en cours") #par exemple.
 
             #on prend lien, la variable qui nous est renvoyée par la page, et on la donne aux fonctions que nous avons définies par ailleurs.
             soup = self.grabpage(lien) #la fonction grabpage retourne une `soup`, donc on dit que `soup` prend le résultat de `grabpage` appliqué à la variable lien.
             comptage = self.comptagemots(soup)
         else:
             print("Il est nécessaire de soumettre une URL")
-        return lien
+        return ("Crawling en cours")
     prendreURL.exposed = True
 
 #recupérer le texte de la page
@@ -43,14 +42,25 @@ class Crawler(object):
         return soup
     grabpage.exposed = True
 
-
+#comptage des mots
     def comptagemots(self, soup):
         songs = (soup.get_text())
         lsongs = [song.replace('"', '').lower() for song in songs.split()]
         freqs = [(- lsongs.count(song), song) for song in set(lsongs)]
         soup = ("\n".join("%-10s : %s" % (n, -f) for f, n in sorted(freqs)))
+        #enrgistrer les 3 1er mots dans un fichier txt
+        monFichier = open("resultatscrawling.txt", "w", encoding="utf-8")
+        monFichier.write("\n".join("%-10s : %s" % (n, -f) for f, n in sorted(freqs)[:3]))
+        monFichier.close()
         return soup
     comptagemots.exposed = True
+
+#récupérer les liens de la page
+
+    
+
+
+
 
 #demarrage du serveur cherrypy
 cherrypy.quickstart(Crawler(), config='server.conf')
