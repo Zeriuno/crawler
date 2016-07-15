84d45be56e76b75d4d71db6ca8b6c31e1b1a8485
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-07-15 19:19:44 +0200

    La création d'une variable de la classe Page déclenche la recherche des liens

--- site crawler/code/Page.py
+++ site crawler/code/Page.py
@@ -1,10 +1,11 @@
-class page(object):
+class Page:
     '''
     Objet pour traiter une page.
 
     Attributs:
         url : pour garder l'adresse de la page.
         soup: pour traiter la page via BeautifulSoup
+        links: liste avec les liens présents dans la page, sans doublons.
     '''
 
     def __init__(self, url):
@@ -13,3 +14,9 @@ class page(object):
         self.url = url
         r = requests.get(self.url)
         self.soup = BeautifulSoup(r.content, "html.parser")
+        # Récupère les liens de la page et les place dans un tableau.
+        self.links = []
+        for link in self.soup.find_all('a'):
+            url = link.get('href')
+            if(url not in self.links):
+                self.links.append(url)
