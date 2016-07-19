f4b4a304b4a62045937e82dd938fa3f02b6ff73e
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-07-20 01:20:16 +0200

    Révision URLWords

--- site crawler/code/URLWords.py
+++ site crawler/code/URLWords.py
@@ -1,18 +1,21 @@
 class URLWords(object):
     '''
-    Objet pour traiter les résultats des analyses des pages.
+    Objet pour traiter les résultats des analyses des pages en vue de les transférer dans le résultat et de les stocker.
 
 
     Attributs:
         address: l'url de la page analysée
-        wordcount: un entier pour le total des mots.
-        wordsitems: une liste avec mots, leur occurrence, leur pourcentage (["salut", 3, 42.86], ["bonjour",  2, 28.57], ["adieu", 2, 28.57])
+        wordlist: une liste avec mots, leur occurrence, leur pourcentage (["salut", 3, 42.86], ["bonjour",  2, 28.57], ["adieu", 2, 28.57])
     '''
 
-    def __init__(self, url=None):
+    def __init__(self, Page):
         '''
-        Pour créer un objet URLWords en passant l'URL en paramètre (la première valeur qu'on a, on la stocke donc directement).
+        Pour créer un objet URLWords en passant la classe Page.
         Exemple d'utilisation:
-        analysePage = URLWords(url)
+        analysePage = URLWords(Page1)
         '''
-        self.address = url
+        self.address = Page.url
+        self.wordlist = Page.wordlist
+
+#fonction pour mettre dans le résultat
+#fonction pour sauvegarder
