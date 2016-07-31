7bdac79c262fcaf178bcd9e16af62617fe6df1ea
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-07-31 16:46:29 +0200

    Test des url complété

--- site crawler/web/cherrypy/Page.py
+++ site crawler/web/cherrypy/Page.py
@@ -31,22 +31,25 @@ class Page:
         self.links = []
         self.wordset = []
         for link in self.soup.find_all('a'): #il faudra améliorer ça en utilisant urllib.parse et urllib.join
-            url = link.get('href')
-            print("J'ai vu ce lien: " + url)
-            urlanalysis = urlparse(url)
-            if urlanalysis.scheme == 'mailto' or urlanalysis.scheme == 'javascript':
-                print("Je tue ce lien: " + url)
-                url = ''
-            if urlanalysis.scheme == '' and urlanalysis.netloc == '':
-                if url[0] == '/':
-                    url = homeparse.scheme + '://' + homeparse.netloc + url
-                else:
-                    url = homeparse.scheme + '://' + homeparse.netloc + '/' + url
-            if urlanalysis.scheme == '' and urlanalysis.netloc != '':
-                url = 'http://' + url
-            if url != '' and url not in self.links:
-                self.links.append(url)
-                print("J'ai pris ce lien :" + url)  # debug
+            try:
+                url = link.get('href')
+                print("J'ai vu ce lien: " + url)
+                urlanalysis = urlparse(url)
+                if url is None or urlanalysis.scheme == 'mailto' or urlanalysis.scheme == 'javascript':
+                    print("Je tue ce lien: " + url)
+                    url = ''
+                if urlanalysis.scheme == '' and urlanalysis.netloc == '':
+                    if url[0] == '/':
+                        url = homeparse.scheme + '://' + homeparse.netloc + url
+                    else:
+                        url = homeparse.scheme + '://' + homeparse.netloc + '/' + url
+                if urlanalysis.scheme == '' and urlanalysis.netloc != '':
+                    url = 'http://' + url
+                if url != '' and url not in self.links:
+                    self.links.append(url)
+                    print("J'ai pris ce lien :" + url)  # debug
+            except TypeError:
+                pass
 
 
     def stopwords(self, lien):
