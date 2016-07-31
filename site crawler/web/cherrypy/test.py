b74fa658570a34e5f4dc92846235fc5abcbbd761
MIMOC2016 <meriyamabane@gmail.Com>
2016-07-31 20:55:21 +0200

    manque test et mise en forme sinon ok pour affichage

--- site crawler/web/cherrypy/test.py
+++ site crawler/web/cherrypy/test.py
@@ -23,18 +23,19 @@ words_level1 = [Page1.wordset[0], Page1.wordset[1], Page1.wordset[2]]
 level1.results = words_level1
 print("RESULTATS DU CRAWLING")
 print("\n")
-print("Liens de la page 1", Page1.links)
 #VERIF print(level1.results)
-print("     Résultats du crawling de la page 1 : ","Mot : ", level1.results[0][2], ", nombre d'occurences du mot : ", level1.results[0][0], ",pourcentage de présence du mot : ", level1.results[0][1],"%")
-print("     Résultats du crawling de la page 1 : ","Mot : ", level1.results[1][2], ", nombre d'occurences du mot : ", level1.results[1][0], ",pourcentage de présence du mot : ", level1.results[1][1],"%")
-print("     Résultats du crawling de la page 1 : ","Mot : ", level1.results[2][2], ", nombre d'occurences du mot : ", level1.results[2][0], ",pourcentage de présence du mot : ", level1.results[2][1],"%")
+print("     Résultats du crawling de la page 1 ")
+print("Liens de la page 1", Page1.links)
+print("Mot : ", level1.results[0][2], ", nombre d'occurences du mot : ", level1.results[0][0], ",pourcentage de présence du mot : ", level1.results[0][1],"%")
+print("Mot : ", level1.results[1][2], ", nombre d'occurences du mot : ", level1.results[1][0], ",pourcentage de présence du mot : ", level1.results[1][1],"%")
+print("Mot : ", level1.results[2][2], ", nombre d'occurences du mot : ", level1.results[2][0], ",pourcentage de présence du mot : ", level1.results[2][1],"%")
 
 # level1.results = Page1.results_level1()  # La fonction renvoie les trois premiers résultats, et ils sont passés dans la liste results
 
 level2_links = []  # ici on mettra tous les liens présents dans toutes les pages
 level2 = []  # contrairement à `level1`, cette variable est une liste. Chaque élément de la liste est un URLWords.
 for index, link in enumerate(Page1.links):  # tous les liens des pages du deuxième niveau sont dans cette liste. On y boucle sur un nombre limité d'éléments.
-    print("Dans la boucle enumerate(Page1.links)")
+    # debug print("Dans la boucle enumerate(Page1.links)")
     if index == 10:  # limitation horizontale: il n'y aura l'analyse que de dix liens pour niveau
         print("index == 10")  # debug
         break
@@ -45,12 +46,10 @@ for index, link in enumerate(Page1.links):  # tous les liens des pages du deuxi�
     for link in Page2.links:  # test pour éviter de mettre plusieurs fois le même lien dans la liste. On ne veut pas mettre à nouveau le lien de la page source ni plusieurs fois le même lien
         if link != Page1.url and link not in level2_links:
             level2_links.append(link)
-            print("Liens de la page 2 ", Page2.links)
         Page2.wordcount()  # de chaque page on compte les mots
         res_lev2 = URLWords(Page2)  # On crée un objet pour chaque page
         res_lev2.results = Page2.find_same_words(level1)  # On garde trace des résultats. S'il n'y a pas de mots qui reviennent 2% ou plus, la liste sera vide.
         level2.append(res_lev2)  # on ajoute le résultat dans le tableau
-
         level3 = []  # comme `level2`, cette variable est une liste. Chaque élément de la liste est un URLWords.
         for index, link in enumerate(level2_links):  # cette fois, troisième itération, on boucle sur les liens trouvés au deuxième niveau.
             if index == 10:
@@ -59,11 +58,31 @@ for index, link in enumerate(Page1.links):  # tous les liens des pages du deuxi�
             #print("3 Encore un objet page")
             # on crée un objet pour chaque lien
             Page3.wordcount()  # de chaque page on compte les mots
-            if level3: #on affiche pas les listes vides
-                print("Liens de la page 3", Page3.links)
         res_lev3 = URLWords(Page3)  # On crée un objet pour chaque page
         res_lev3.results = Page3.find_same_words(level1)  # On garde trace des résultats. S'il n'y a pas de mots qui reviennent 2% ou plus, la liste sera vide.
         level3.append(res_lev3)  # on ajoute le résultat dans le tableau
 
         crawling = [level1, level2, level3]  # Tous les résultats dans une seule variable. `level1` est un URLWords, `level2` et `level3` sont des tableaux de 10 éléments de URLWords chacun (la limite horizontale imposée avec `index == 10`).
         # show_results(crawling)  # on pourrait appeller la fonction qui fait l'affichage des résultats
+print("     Résultats du crawling de la page 2 ")
+if res_lev2.results:
+   print("Liens de la page 2 ", Page2.links)
+else:
+    print("Pas de liens pour la page 2") # on affiche pas les listes vides
+
+#verif print(res_lev2.results)
+print("Mot : ", res_lev2.results[0][2], ", nombre d'occurences du mot : ", res_lev2.results[0][0], ",pourcentage de présence du mot : ", res_lev2.results[0][1],"%")
+print("Mot : ", res_lev2.results[1][2], ", nombre d'occurences du mot : ", res_lev2.results[1][0], ",pourcentage de présence du mot : ", res_lev2.results[1][1],"%")
+
+print("\n")
+
+print("     Résultats du crawling de la page 3 ")
+
+if res_lev3.results:
+    print("Liens de la page 3", Page3.links)
+else:
+    print("Pas de liens pour la page 3") # on affiche pas les listes vides
+
+# verif print(res_lev3.results)
+print("Mot : ", res_lev2.results[0][2], ", nombre d'occurences du mot : ", res_lev2.results[0][0], ",pourcentage de présence du mot : ", res_lev2.results[0][1],"%")
+print("Mot : ", res_lev2.results[1][2], ", nombre d'occurences du mot : ", res_lev2.results[1][0], ",pourcentage de présence du mot : ", res_lev2.results[1][1],"%")
