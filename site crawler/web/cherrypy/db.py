87c2e4fdc7fa3736dd7a935803986b18eb362a26
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-08-02 00:57:17 +0200

    __init__ et insertPage testées en mode interactif

--- site crawler/web/cherrypy/db.py
+++ site crawler/web/cherrypy/db.py
@@ -8,33 +8,33 @@ class db:
     def __init__(self, db:object) -> object:
         """
         """
-
-        self.pymysql.connect(host='localhost', port= 3306, user="", password="", database="", charset='utf8')
+        self.conn = pymysql.connect(host='localhost', port= 3306, user="mimocrawlerusr", password="yolo", database="mimocrawlerdb", charset='utf8')
 
     def insertPage(Page):
         """
         Insère une page analysée dans la base.
         """
-        cur = self.cursor()
-        # date = date() # obtenir la date du jour et la mettre dans une variable
-        cur.execute("insert into url (url,titre,descrip,effort,price,level,view,rate) values ('"+url+"','"+titre+"','"+descrip+"','"+effort+"','"+price+"','"+level+"','"+view+"','"+rate+"')")
+        curs = self.conn.cursor()
+        curs.execute("INSERT INTO url (url, date) VALUES ('"+Page.url+"', CURDATE())")
+
+        self.conn.commit()
 
     def readPage(Page):
 
 
-Pour la lecture:
-    ```
-    for row in result:
-          mooc.append([row["url"],row["titre"],row["descrip"],row["effort"],row["price"],row["level"],row["view"],row["rate"],score])
-    ```
-
-/*
-INSERT INTO url(url, date)
-+ Page.url +, CURDATE());
-
-La requête pour obtenir les mots d'une page est
-
-SELECT item, occurrences, percentage
-FROM  words
-WHERE url = $item ;
-*/
+# Pour la lecture:
+#     ```
+#     for row in result:
+#           mooc.append([row["url"],row["titre"],row["descrip"],row["effort"],row["price"],row["level"],row["view"],row["rate"],score])
+#     ```
+#
+# /*
+# INSERT INTO url(url, date) values
+# ('""+Page.url+"', CURDATE());
+#
+# La requête pour obtenir les mots d'une page est
+#
+# SELECT item, occurrences, percentage
+# FROM  words
+# WHERE url = $item ;
+# */
