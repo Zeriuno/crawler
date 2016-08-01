b1dcdb13dc82db3d16c46f8959910c2e8c2f2627
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-08-01 18:27:42 +0200

    Insertion du résultat d'une page, première hypothèse

--- site crawler/web/cherrypy/db.py
+++ site crawler/web/cherrypy/db.py
@@ -10,3 +10,11 @@ class db:
         """
 
         self.pymysql.connect(host='localhost', port= 3306, user="", password="", database="", charset='utf8')
+
+    def insertPage(Page):
+        """
+        Insère une page analysée dans la base.
+        """
+        cur = self.cursor()
+        # date = date() # obtenir la date du jour et la mettre dans une variable
+        cur.execute("insert into url (url,titre,descrip,effort,price,level,view,rate) values ('"+url+"','"+titre+"','"+descrip+"','"+effort+"','"+price+"','"+level+"','"+view+"','"+rate+"')")
