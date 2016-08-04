aa18520e4976ff7dbf855fb001a9c72bac0527dd
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-08-04 20:24:29 +0200

    Niveau 3 sorti de la boucle for du niveau 2

--- site crawler/web/cherrypy/crawl.py
+++ site crawler/web/cherrypy/crawl.py
@@ -41,12 +41,12 @@ def analysis(lien, largeur):
             Page2.wordcount()  # de chaque page on compte les mots
             res_lev2 = URLWords(Page2)  # On crée un objet pour chaque page
             res_lev2.results = Page2.find_same_words(level1)  # On garde trace des résultats. S'il n'y a pas de mots qui reviennent X% ou plus, la liste sera vide.
-            level2.append(res_lev2)  # on ajoute le résultat dans le tableau
-            level3 = []  # comme `level2`, cette variable est une liste. Chaque élément de la liste est un URLWords.
-            for link in level2_links[:20]:  # cette fois, troisième itération, on boucle sur les liens trouvés au deuxième niveau.
-                Page3 = Page(link)
-                # print("3 Encore un objet page")  # on crée un objet pour chaque lien
-                Page3.wordcount()  # de chaque page on compte les mots
+            level2.append(res_lev2)  # on ajoute le résultat dans la liste
+        level3 = []  # comme `level2`, cette variable est une liste. Chaque élément de la liste est un URLWords.
+        for link in level2_links[:20]:  # cette fois, troisième itération, on boucle sur les liens trouvés au deuxième niveau.
+            Page3 = Page(link)
+            # print("3 Encore un objet page")  # on crée un objet pour chaque lien
+            Page3.wordcount()  # de chaque page on compte les mots
             res_lev3 = URLWords(Page3)  # On crée un objet pour chaque page
             res_lev3.results = Page3.find_same_words(level1)  # On garde trace des résultats. S'il n'y a pas de mots qui reviennent 2% ou plus, la liste sera vide.
             level3.append(res_lev3)  # on ajoute le résultat dans le tableau
