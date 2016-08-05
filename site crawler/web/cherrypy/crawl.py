7cb0c44bbb78013e9ca6a3d43f592e2809c2a637
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-08-05 08:57:18 +0200

    Correction commentaire

--- site crawler/web/cherrypy/crawl.py
+++ site crawler/web/cherrypy/crawl.py
@@ -48,10 +48,10 @@ def analysis(lien, largeur):
         # print("3 Encore un objet page")  # on crée un objet pour chaque lien
         Page3.wordcount()  # de chaque page on compte les mots
         res_lev3 = URLWords(Page3)  # On crée un objet pour chaque page
-        res_lev3.results = Page3.find_same_words(level1)  # On garde trace des résultats. S'il n'y a pas de mots qui reviennent 2% ou plus, la liste sera vide.
+        res_lev3.results = Page3.find_same_words(level1)  # On garde trace des résultats. S'il n'y a pas de mots qui reviennent X% ou plus, la liste res_lev3 sera vide.
         level3.append(res_lev3)  # on ajoute le résultat dans le tableau
 
-    crawling = [level1, level2, level3]  # Tous les résultats dans une seule variable. `level1` est un URLWords, `level2` et `level3` sont des tableaux de 10 éléments de URLWords chacun (la limite horizontale imposée avec `index == 10`).
+    crawling = [level1, level2, level3]  # Tous les résultats dans une seule variable. `level1` est un URLWords, `level2` et `level3` sont des listes d'éléments URLWords (la limite max horizontale imposée avec `largeur` et ses ajustements).
         # show_results(crawling)  # on pourrait appeller la fonction qui fait l'affichage des résultats
     print("     Résultats du crawling de la page 2 ")
     if res_lev2.results:
