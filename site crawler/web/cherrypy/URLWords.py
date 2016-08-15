8ea2eefd01ebcf1804c27a0bb7e595fd42eb86bc
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-08-15 17:57:01 +0200

    PEP8

--- site crawler/web/cherrypy/URLWords.py
+++ site crawler/web/cherrypy/URLWords.py
@@ -1,19 +1,19 @@
 class URLWords(object):
-    '''
+    """
     Objet pour traiter les résultats des analyses des pages en vue de les afficher.
 
 
     Attributs:
         address: l'url de la page analysée
         results: une liste avec mots, leur occurrence, leur pourcentage ([3, 42.86, 'salut'], [2, 28.57, 'bonjour'], [2, 28.57, 'adieu'])
-    '''
+    """
 
     def __init__(self, Page: object) -> object:
-        '''
+        """
         Pour créer un objet URLWords en passant la classe Page.
         Exemple d'utilisation:
         analysePage = URLWords(Page1)
-        '''
+        """
         self.address = Page.url
         self.results = []
 
