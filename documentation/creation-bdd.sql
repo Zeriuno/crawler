967588daffe35c0ab4f7fb61496773568779d942
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-08-15 11:24:32 +0200

    Nouvelle structure BDD

--- documentation/creation-bdd.sql
+++ documentation/creation-bdd.sql
@@ -1,16 +1,22 @@
 USE mimocrawlerdb;
-DROP TABLE IF EXISTS url, words;
+DROP TABLE IF EXISTS url, words, links;
 
 CREATE TABLE url(
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  idurl INT AUTO_INCREMENT PRIMARY KEY,
   url VARCHAR(300) NOT NULL,
-  date DATE NOT NULL
+  date DATETIME NOT NULL
 );
 
 CREATE TABLE words(
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  url VARCHAR(300) references url(url),
-  item VARCHAR(300),
-  occurrences INT,
+  idword INT AUTO_INCREMENT PRIMARY KEY,
+  idurl INT references url(idurl),
+  item VARCHAR(300) NOT NULL,
+  occurrences INT NOT NULL,
   percentage FLOAT
 );
+
+CREATE TABLE links(
+  idlink INT AUTO_INCREMENT PRIMARY KEY,
+  link VARCHAR(300) NOT NULL,
+  idurl INT references url(idurl)
+)
