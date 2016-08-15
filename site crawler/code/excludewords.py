d417be88e074b4815946f03ee69259859a1bce98
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-08-15 11:12:22 +0200

    Ménage

--- site crawler/code/excludewords.py
+++
@@ -1,8 +0,0 @@
-import nltk
-nltk.download()
-
-from nltk.corpus import stopwords
-
-print (stopwords.words("french"))
-words = [w for w in words if not w in stopwords.words("french")]
-print (words)
