42f329753634b2b23063290bfb4845b6b6e2569d
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-08-18 09:16:59 +0200

    Fonction fausse 'stopwords()' supprimée

--- web/Page.py
+++ web/Page.py
@@ -50,19 +50,6 @@ class Page:
                 pass
 
 
-    # def stopwords(self, lien):
-    #     """
-    #     Filtrer les mots
-    #     """
-    #     #dictionnaire français des mots à exclure
-    #     stop_words = set(stopwords.words("french"))
-    #     words = word_tokenize(lien)
-    #     filtered_phrase = []
-    #     for w in words:
-    #         if w not in stop_words:
-    #             lien = filtered_phrase.append(w)
-
-
     def wordcount(self):
         """
         Fonction qui construit le compte des mots de la page. Elle est appellée avec `NomObjetPage.wordcount()`
